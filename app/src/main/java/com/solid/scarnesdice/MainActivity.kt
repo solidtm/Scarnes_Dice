58770c13823a95de7eb7cbce2ebf63784714e2c4
Richard <richardebboh@gmail.com>
2022-01-24 04:34:22 -0800

    implemeted onclick listener for imageView

--- app/src/main/java/com/solid/scarnesdice/MainActivity.kt
+++ app/src/main/java/com/solid/scarnesdice/MainActivity.kt
@@ -2,6 +2,7 @@ package com.solid.scarnesdice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.solid.scarnesdice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,8 +18,21 @@ class MainActivity : AppCompatActivity() {
          var compOverallScore: Int
          var compTurnScore: Int
 
-         binding.rollButton.setOnClickListener{
 
+
+
+         binding.rollButton.setOnClickListener{
+            val random = (1..6).random()
+            var imageView = binding.diceImageView
+
+            when(random){
+                1 ->  imageView.setImageResource(R.drawable.dice1)
+                2 ->  imageView.setImageResource(R.drawable.dice2)
+                3 ->  imageView.setImageResource(R.drawable.dice3)
+                4 ->  imageView.setImageResource(R.drawable.dice4)
+                5 ->  imageView.setImageResource(R.drawable.dice5)
+                6 ->  imageView.setImageResource(R.drawable.dice6)
+            }
          }
 
     }
