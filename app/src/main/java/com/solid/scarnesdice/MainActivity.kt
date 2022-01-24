09e0cd7ac96362c07bb6143facfd63f8eccf11fe
Richard <richardebboh@gmail.com>
2022-01-24 03:59:08 -0800

    Project Setup and main activity file created

---
+++ app/src/main/java/com/solid/scarnesdice/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.solid.scarnesdice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.solid.scarnesdice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+     private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+         var userOverallScore: Int
+         var usersTurnScore: Int
+         var compOverallScore: Int
+         var compTurnScore: Int
+
+
+    }
+}
